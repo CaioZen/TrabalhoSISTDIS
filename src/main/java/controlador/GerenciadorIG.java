ef56df8cab65f4d13125afa0079377f872ab2eb6
Caio Zen Gonçalves <2023122760123@cefetes.br>
2025-08-20 14:21:46 -0300

    :tada: init

---
+++ src/main/java/controlador/GerenciadorIG.java
@@ -0,0 +1,53 @@
+package controlador;
+
+import java.awt.Frame;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import visao.DialogUsuario;
+import visao.FramePrincipal;
+
+public class GerenciadorIG {
+
+    private static final GerenciadorIG instancia = new GerenciadorIG();
+    
+    private FramePrincipal framePrincipal = null;
+    private DialogUsuario dialogUsuario = null;
+
+    public static GerenciadorIG getInstancia() {
+        return instancia;
+    }
+
+    private GerenciadorIG() {
+
+    }
+
+    public JDialog abrirJanela(java.awt.Frame parent, JDialog dlg, Class classe) {
+        if (dlg == null) {
+            try {
+                dlg = (JDialog) classe.getConstructor(Frame.class, boolean.class).newInstance(parent, true);
+            } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                JOptionPane.showMessageDialog(parent, "Erro ao abrir a janela " + classe.getName() + ". " + ex.getMessage());
+            }
+        }
+        dlg.setVisible(true);
+        return dlg;
+    }
+    
+    public void abrirFramePrincipal(){
+        if(framePrincipal == null){
+            framePrincipal = new FramePrincipal();
+        }
+        framePrincipal.setVisible(true);
+    }
+    
+    public void abrirCadastro(){
+        dialogUsuario = (DialogUsuario) abrirJanela(framePrincipal, dialogUsuario, DialogUsuario.class);
+    }
+    
+    public static void main(String args[]){
+        GerenciadorIG gerIG = GerenciadorIG.getInstancia();
+        gerIG.abrirFramePrincipal();
+    }
+}
