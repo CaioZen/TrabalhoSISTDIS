62dbb6f210bf89428e0dcaac42feba115bb227dd
CaioZen <caiozengoncalves@gmail.com>
2025-09-01 18:44:21 -0300

    :construction: Usr Monitoring WIP

---
+++ src/main/java/controlador/MonitorUsr.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author Caio
+ */
+public class MonitorUsr extends Thread{
+
+    public MonitorUsr() {
+    }
+
+    @Override
+    public void run() {
+        super.run(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
